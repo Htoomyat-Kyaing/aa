7f4a26eea8d4988b14698ac222570cabdb14d655
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-02-27 12:40:46 +0630

    background

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -7,19 +7,21 @@ const Hero = () => {
   const images = ["/hero/1.jpg", "/hero/2.jpg", "/hero/3.jpg", "/hero/4.jpg"];
 
   const imageVariants = {
-    hidden: { scale: 1 },
-    visible: { scale: [1, 1.2] },
+    hidden: { scale: 1, opacity: 0 },
+    visible: { scale: [1, 1.2], opacity: 1 },
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
-    }, 4000); // Change image every 5 seconds
+    }, 5000); // Change image every 5 seconds
 
     return () => clearInterval(interval); // Clean up interval on unmount
   }, []);
   return (
-    <div className="relative flex items-center justify-center h-screen overflow-hidden">
+    // z-index -20
+    <div className="relative flex items-center justify-center h-screen overflow-hidden -z-20">
+      {/* z-index 0 */}
       <AnimatePresence mode="wait">
         <motion.img
           key={currentImage} // Add key to force re-render on image change
@@ -28,35 +30,42 @@ const Hero = () => {
           className="object-cover w-full h-screen transition-transform duration-[8000ms] ease-linear absolute"
           variants={imageVariants}
           transition={{
+            delay: 0.3,
             duration: 4,
+            type: "spring",
             opacity: {
-              duration: 1,
-              ease: "easeIn",
-            },
-          }}
-          initial="hidden"
-          animate="visible"
-          // exit={{ opacity: 0 }}
-        />
-
-        {/* Preload next image before previous image?? */}
-        {/* <motion.img
-          key={currentImage} 
-          src={images[(currentImage + 1) % images.length]}
-          alt="Placeholder"
-          className="object-cover w-full h-screen transition-transform duration-[8000ms] ease-linear absolute"
-          variants={imageVariants}
-          transition={{
-            duration: 4,
-            opacity: {
-              duration: 1,
-              ease: "easeIn",
+              duration: 0.7,
+              ease: "easeInOut",
             },
           }}
           initial="hidden"
           animate="visible"
           exit={{ opacity: 0 }}
-        /> */}
+        />
+      </AnimatePresence>
+
+      {/* Preload next image before previous image?? */}
+      {/* z-index -10 */}
+      <AnimatePresence>
+        <motion.img
+          key={currentImage} // Add key to force re-render on image change
+          src={images[currentImage]}
+          alt="Placeholder"
+          className="absolute object-cover w-full h-screen -z-10"
+          // variants={imageVariants}
+          // transition={{
+          //   delay: 0.5,
+          //   duration: 4,
+          //   type: "spring",
+          //   opacity: {
+          //     duration: 0.7,
+          //     ease: "easeInOut",
+          //   },
+          // }}
+          // initial="hidden"
+          // animate="visible"
+          // exit={{ opacity: 0 }}
+        />
       </AnimatePresence>
     </div>
   );
